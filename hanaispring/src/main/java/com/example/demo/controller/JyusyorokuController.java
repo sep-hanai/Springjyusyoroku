4ef37efb03d7dd473f96b1b8325f6b68696c4b8f
Hanai7710sep <hanainanami1198@se-project.co.jp>
2020-07-16 10:25:52 +0900

    7/16、自宅、クラス等作成、書き込み

---
+++ hanaispring/src/main/java/com/example/demo/controller/JyusyorokuController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.service.JyusyorokuService;
+
+@Controller
+public class JyusyorokuController {
+
+	 @Autowired
+
+	JyusyorokuService jyusyorokuService;
+
+
+}
